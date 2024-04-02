cef754712e6141b8c0b1feb13cace2541fd2717b
Seo <seojong789@naver.com>
2024-04-02 21:43:06 +0900

    FEAT: 쿼리 매개변수 + 인증에서 쿼리매개변수를 통해 mode 확인

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,14 +1,21 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  // const [isLogin, setIsLogin] = useState(true);
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  // function switchAuthHandler() {
+  //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  // }
+
+  /* 
+  위 코드를 쿼리 매개변수 로직으로 변경 = useSearchParams 훅 사용
+  state처럼 두 번째 파라미터는 업데이트 함수인데, 해당 Form에서는 mode?=${} 를 통해 동적으로 관리하기 때문에 업데이트 함수는 필요없다.
+  */
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get('mode') === 'login';
+  console.log(searchParams);
 
   return (
     <>
@@ -23,9 +30,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          {/* <button onClick={switchAuthHandler} type="button">
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </button> */}
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
           <button>Save</button>
         </div>
       </Form>
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,34 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+// request : AuthForm으로 전송되는 양식에 대해 접근이 필요하기 때문에 사용(await request.formData();)
+/* 
+request는 쿼리 매개변수에 따라서 다르게 보내야 함. (login, signup)
+그러나, action 함수 내부에서 useSearchParams 사용 불가능.
+-> 브라우저가 제공하는 URL 생성자 함수 사용.
+*/
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login'; // AuthForm에서 쿼리 매개변수를 ?mode=로 지정했음.
+
+  // 사용자가 주소를 강제로 ?mode=abc 등으로 바꾸는 걸 방지하기 위한 유효성 검사.
+  if (mode !== 'signup' && mode !== 'login') {
+    throw json({ message: '지원하지 않는 모드입니다.' }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'), // email, password : AuthForm에서 입력의 name 프롭에 해당함.
+    password: data.get('password'),
+  };
+
+  // mode 변수를 통해 어떤 mode인지 알 수 있으므로, 해당 mode에 맞게 요청 가능 (mode는 signup 아니면 login)
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST', // 로그인, 회원가입은 둘 다 POST
+    header: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+}
